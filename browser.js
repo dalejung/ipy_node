a0648c5a969da35455c7717885ceaa7d803c1edc
Dale Jung <dale@dalejung.com>
2013-07-20 18:10:47 -0400

    repose Kernel

--- browser.js
+++ browser.js
@@ -1,7 +1,11 @@
 var IPython = require('./lib/ipython.js');
 var Bridge = require('./lib/bridge.js');
+var Kernel = require('./lib/kernel.js');
 
 ipy_node = {
   Bridge : Bridge, 
+  Kernel : Kernel, 
   IPython : IPython
 }
+
+module.exports = ipy_node;
