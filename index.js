8d02dc0ba1dacc77edff88e8dcd0ceed68b60593
Dale Jung <dale@dalejung.com>
2013-07-21 03:56:08 -0400

    update kernel

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var IPython = require('./lib/ipython.js');
 var Bridge = require('./lib/bridge.js');
+var Kernel = require('./lib/kernel.js');
 
 module.exports.IPython = IPython;
 module.exports.Bridge = Bridge;
+module.exports.Kernel = Kernel;
 
 module.exports.magic = require('./lib/magic.js');
