6ccead1f8ce013273bcf86283a669bba1610661d
Dale Jung <dale@dalejung.com>
2014-03-07 22:57:21 -0500

    change Kernel.pull to retain variable name

--- startup.py
+++ startup.py
@@ -2,17 +2,19 @@ import pandas as pd
 from pandas import json
 from IPython.display import JSON
 
-def dataframe_json(df):
+def dataframe_json(df, obj_name):
     data = {}
     for k, v in df.iteritems():
         data[k] = v.values
     data['index'] = df.index
     data['__repr__'] = repr(df)
+    data['__obj_name__'] = obj_name
     return json.dumps(data)
 
-def _to_json(obj):
+def _to_json(obj_name):
+    obj = globals()[obj_name]
     if isinstance(obj, pd.DataFrame):
-        return dataframe_json(obj)
+        return dataframe_json(obj, obj_name)
     if isinstance(obj, dict):
         jdict = {}
         for k, v in obj.iteritems():
@@ -28,7 +30,7 @@ def json_dict(dct):
     items = []
     for k, v in dct.iteritems():
         items.append("\"{k}\":{v}".format(k=k, v=v))
-    return "{" + ','.join(items) + "}" 
+    return "{" + ','.join(items) + "}"
 
-def to_json(obj):
-    return JSON(_to_json(obj));
+def to_json(obj_name):
+    return JSON(_to_json(obj_name));
